df2d1b077b7a2ae52462a268eec9ce1add3c2435
LiFei <lifei@yongyida.com>
2017-10-27 15:33:56 +0800

    优化SP5509

--- drivers/misc/mediatek/imgsensor/src/mt8163/sp5509_mipi_raw/hi556mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt8163/sp5509_mipi_raw/hi556mipiraw_Sensor.c
@@ -243,7 +243,7 @@ static imgsensor_info_struct imgsensor_info = {
 	.sensor_interface_type = SENSOR_INTERFACE_TYPE_MIPI,//sensor_interface_type
     .mipi_sensor_type = MIPI_OPHY_NCSI2, //0,MIPI_OPHY_NCSI2;  1,MIPI_OPHY_CSI2
     .mipi_settle_delay_mode = 1,//0,MIPI_SETTLEDELAY_AUTO; 1,MIPI_SETTLEDELAY_MANNUAL
-	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_Gb,//sensor output first pixel color
+	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_B,//sensor output first pixel color
 	.mclk = 24,//mclk value, suggest 24 or 26 for 24Mhz or 26Mhz
 	.mipi_lane_num = SENSOR_MIPI_2_LANE,//mipi lane num
 	.i2c_addr_table = {0x40,0x50,0xff},//record sensor support all write id addr, only supprt 4must end with 0xff
@@ -698,7 +698,7 @@ write_cmos_sensor(0x3068,0xf800);
 write_cmos_sensor(0x306a,0xf876);
 write_cmos_sensor(0x006c,0x0000);
 write_cmos_sensor(0x005e,0x0200);
-write_cmos_sensor(0x000e,0x0100);
+write_cmos_sensor(0x000e,0x0000);
 write_cmos_sensor(0x0e0a,0x0001);
 write_cmos_sensor(0x004a,0x0100);
 write_cmos_sensor(0x004c,0x0000);
